45e4afc3b4c82bcfdd8978d5caa2fe3533616686
Nihal Rajpal <suryakant.rajpal@gmail.com>
2024-04-03 19:12:41 +0530

    Bug fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +22,7 @@ class DatabaseSeeder extends Seeder
             $user=\App\Models\User::factory()->create([
                 'name' => 'Admin',
                 'email' => 'admin@example.com',
-                'password' => 'admin@123',
+                'password' => Hash::make('admin@123'),
             ]);
             $user->assignRole('admin');
         });
