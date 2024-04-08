0099601d795e2bfd7e181264d0e76022d887461a
Parth <parthtpujara@gmail.com>
2024-04-08 11:48:45 +0000

    Admin js file add

---
+++ public/assets/js/admin.js
@@ -0,0 +1,8 @@
+if (screen.width < 775) {
+    sideBar.classList.add("mobile");
+} else {
+    sideBar.classList.remove("mobile");
+}
+function sideNavResponsive() {
+        sideBar.classList.toggle("mobile");
+}
